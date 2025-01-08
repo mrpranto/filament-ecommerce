242b979ff350d110865eaa57577bf5f9a71e8425
mrpranto <prantoabir1@gmail.com>
2025-01-08 06:40:01 +0600

    Discount Functionality done

--- app/Filament/Resources/DiscountResource.php
+++ app/Filament/Resources/DiscountResource.php
@@ -9,9 +9,11 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use function Laravel\Prompts\alert;
 
 class DiscountResource extends Resource
 {
@@ -33,14 +35,25 @@ class DiscountResource extends Resource
                 Forms\Components\TextInput::make('amount')
                     ->required()
                     ->numeric()
-                    ->suffix('BDT'),
+                    ->suffix(fn ($get) => $get('amount_suffix') ?? 'BDT'),
 
                 Forms\Components\Select::make('type')
+                    ->live()
+                    ->required()
                     ->options([
-                        'percentage' => 'Percentage',
-                        'flat' => 'Flat',
+                        'percentage' => 'Percentage (%)',
+                        'flat' => 'Flat (BDT)',
                     ])
-                    ->native(false),
+                    ->native(false)
+                    ->afterStateUpdated(function ($set, $state) {
+                        if ($state === 'flat') {
+                            $set('amount_suffix', 'BDT');
+                        }else{
+                            $set('amount_suffix', '%');
+                        }
+                    }),
+                Forms\Components\Toggle::make('status')
+                    ->default(true)
             ]);
     }
 
@@ -52,9 +65,12 @@ class DiscountResource extends Resource
                     ->searchable(),
 
                 Tables\Columns\TextColumn::make('amount')
+                    ->searchable()
                     ->label('Discount')
                     ->sortable()
                     ->getStateUsing(fn ($record) => $record->type == 'percentage' ? $record->amount.' %' : $record->amount.' BDT'),
+
+                Tables\Columns\ToggleColumn::make('status'),
 
                 Tables\Columns\TextColumn::make('created_by.name')
                     ->numeric()
@@ -75,14 +91,77 @@ class DiscountResource extends Resource
                     ->sortable(),
             ])
             ->filters([
-                //
+
+                Filter::make('status')
+                    ->form([
+                        Forms\Components\Radio::make('status')
+                            ->inline()
+                            ->options([
+                                '1' => 'Active',
+                                '0' => 'Inactive',
+                            ])
+                    ])->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(isset($data['status']), function (Builder $query) use ($data) {
+                                return $query->where('status', $data['status']);
+                            });
+                    })
+                    ->indicateUsing(function ($data) {
+                        if (isset($data['status'])) {
+                            $status = $data['status'] === '1' ? 'Active' : 'Inactive';
+                            return "Filtered by status {$status}";
+                        }
+                    }),
+
+                Filter::make('created_at')
+                    ->form([
+                        Forms\Components\DatePicker::make('start_date'),
+                        Forms\Components\DatePicker::make('end_date'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['start_date'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['end_date'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    })
+                    ->indicateUsing(function ($data) {
+
+                        $string = '';
+                        if (isset($data['start_date'])) {
+                            $string .= " Start Date = {$data['start_date']}";
+                        }
+                        if (isset($data['end_date'])) {
+                            $string .= " & End Date = {$data['end_date']}";
+                        }
+                        return $string;
+
+                    })
             ])
+            ->filtersTriggerAction(
+                fn(Tables\Actions\Action $action) => $action
+                    ->button()
+                    ->label('Filter'),
+            )
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make()
+                        ->slideOver()
+                        ->successNotificationTitle('Discount updated successfully !')
+                        ->modalIcon('heroicon-o-pencil'),
+
+                    Tables\Actions\DeleteAction::make()
+                        ->successNotificationTitle('Discount deleted successfully !')
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make()
+                        ->successNotificationTitle('Selected Discount deleted successfully !'),
                 ]),
             ]);
     }
